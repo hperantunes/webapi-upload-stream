d926e34035bec8007c7025ddb1ac2a82fd2ce25d
Hilton Perantunes <hperantunes@gmail.com>
2016-06-16 22:40:47 -0400

    Initial files

---
+++ src/WebApiFrom/Controllers/FromController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.Net.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebApiFrom.Controllers
+{
+    [Route("api/[controller]")]
+    public class FromController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "From API is alive!";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]string path)
+        {
+            if (path == null)
+            {
+                return BadRequest("path cannot be null");
+            }
+            try
+            {
+                using (var stream = new FileStream(path, FileMode.Open))
+                {
+                    var content = new StreamContent(stream);
+                    content.Headers.Clear();
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    content.Headers.ContentLength = stream.Length;
+
+                    var client = new HttpClient();
+                    var response = await client.PostAsync("http://localhost:58449/api/to", content);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return BadRequest(response.ReasonPhrase);
+                    }
+                }
+            }
+            catch
+            {
+                return BadRequest("error opening file");
+            }
+            return Ok();
+        }
+    }
+}
---
+++ src/WebApiTo/Controllers/ToController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "To API is alive!";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                using (var stream = System.IO.File.Create(@"C:\temp\newfile.pdf", 1024 * 64))
+                {
+                    await Request.Body.CopyToAsync(stream);
+                }
+            }
+            catch
+            {
+                return BadRequest("can't save file");
+            }
+            return Ok();
+        }
+    }
+}
